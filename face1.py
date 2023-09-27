609b1a0e7f70b18c5fd8d14de32601be8725f555
bandaruvenkatadineshkumar <146200349+bandaruvenkatadineshkumar@users.noreply.github.com>
2023-09-27 19:31:58 +0530

    Add files via upload

---
+++ face1.py
@@ -0,0 +1,19 @@
+import cv2
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+def detect(gray, frame):
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+		roi_gray = gray[y:y + h, x:x + w]
+		roi_color = frame[y:y + h, x:x + w]
+	return frame
+video_capture = cv2.VideoCapture(0)
+while video_capture.isOpened():
+	_, frame = video_capture.read()	
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 	
+	canvas = detect(gray, frame)				
+	cv2.imshow('Video', canvas)				
+	if cv2.waitKey(1) & 0xff == ord('q'):			
+		break
+video_capture.release()								
+cv2.destroyAllWindows()
